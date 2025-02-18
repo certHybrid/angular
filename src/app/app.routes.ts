a5500b537bead675be61ceddf825c242f6a268d2
Olaoluwa Ilesanmi <hybrid@Olaoluwas-MacBook-Air.local>
2025-02-18 11:15:55 +0100

    show more or less button with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,13 +10,13 @@ import { SignupComponent } from './Ass/signup/signup.component';
 import { ListComponent } from './list/list.component';
 
 export const routes: Routes = [
-    {path: "profile", component: ProfileComponent, canActivate:[authGuard]},
+    {path: "profile", component: ProfileComponent},
     {path: "admin", children:[
         {path: "dashboard", component:DashboardComponent},
         {path: "transaction", component:TransactionComponent}
     ]},
     {path: "view/:id", component:ViewComponent},
-    {path: "login", component:LoginComponent},
+    {path: "login", component:LoginComponent, canActivate:[authGuard]},
     {path: "dashbaord", component:DashboardComponent},
     {path: "signup", component:SignupComponent},
     {path: "list", component:ListComponent}
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -4,33 +4,47 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Service1Service } from '../services/service1.service';
 import { PostService } from '../services/post.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, FormsModule],
+  imports: [CommonModule, HttpClientModule, FormsModule, RouterLink],
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],  // Notice "styleUrls" instead of "styleUrl",
   providers:[PostService]
 })
 export class ListComponent {
   // countries: any[] = [];
-  files: any[]=[];
+  public loading:boolean =false;
+  public files: Array<{userId:number, title:string, body:string, id:number}> =[];
+  public showFullText: boolean[]=[];
+
 
   constructor(private axios: HttpClient,public myUser: Service1Service,public allPost:PostService){}  // Changed 'axios' to 'httpClient' for clarity.
   ngOnInit() {
+    this.fetchAllpost()
+  }
+
+  fetchAllpost(){
+    this.loading = true;
     this.allPost.allPost().subscribe
-      (data=> {
-        // console.log(data);
-        // console.log(data.map((pers: any) => pers));
-        
-        this.files = data.map((pers: any) => pers);
-        console.log(this.files);
-      },
-      (error) => {
-        console.log(error);
-      } 
-    );
+    (data=> {
+      
+      this.files = data.map((pers: any) => pers);
+      this.loading=false;
+      console.log(this.files);
+      this.showFullText = new Array(this.files.length).fill(false);
+
+    },
+    (error) => {
+      console.log(error);
+    } 
+  );
+
+  }
+  showmore(index:number){
+this.showFullText[index] = !this.showFullText[index]
   }
 }
 
